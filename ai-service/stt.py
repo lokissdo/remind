8a1710d87ab6695fa822206870b09dbe2348b367
hung <dokhaihung2003@gmai.com>
2024-07-20 11:45:15 +0700

    modify stt

--- ai-service/stt.py
+++ ai-service/stt.py
@@ -11,20 +11,21 @@ AudioSegment.converter  = FFMPEG_PATH
 
 sttModel = deepspeech.Model(STT_MODEL_PATH)
 sttModel.enableExternalScorer(LM_PATH)
-# sttModel.setScorerBeamWidth(BEAM_WIDTH)
+#sttModel.setScorerBeamWidth(BEAM_WIDTH)
 
 
 def convert_audio(file):
     # Load audio file
-    audio = AudioSegment.from_mp3(file)
+    audio = AudioSegment.from_file(file)
 
-    # Convert stereo to mono
+    #Convert stereo to mono
     audio = audio.set_channels(1)
 
-    # Change sample rate to 16000Hz
+    # # Change sample rate to 16000Hz
     audio = audio.set_frame_rate(16000)
 
     return audio
+
 
 def transcribe(file):
         audio = convert_audio(file)
@@ -40,11 +41,12 @@ def transcribe(file):
 
         return text
 
-mp3_file_path ="./test.mp3"
+# mp3_file_path ="./test_convert.mp3"
+file_path = "./sample.mp3"
 # load mp3 file using os and transcribe
 
-if os.path.exists(mp3_file_path):
-    print(transcribe(mp3_file_path))
+if os.path.exists(file_path):
+    print(transcribe(file_path))
 else:
     print("Audio file not found.")
 
